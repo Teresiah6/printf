e2d96712488213d97ddade44a07187c2bb2037fb
teddykarisa <teddykarisachaitkc@gmail.com>
2022-10-23 08:21:02 -0700

    git correction

--- _printf.c
+++ _printf.c
@@ -1,52 +1,223 @@
 #include "main.h"
+
 #include <stdarg.h>
-#include <stdio.h>
+
+
+
 /**
- * _printf - entry point
- * @format: character string
- * Return: int
+
+ * _printf - formatted output conversion and print data.
+
+ * @format: input string.
+
+ * check_format - checks if there is a valid format specifier
+
+ * @format: possible valid format specifier
+
+ *
+
+ * Return: number of chars printed.
+
+ * Return: pointer to valid function or NULL
+
  */
-int _printf(const char *format, ...)
+
+
+
+int (*check_format(const char *format))(va_list)
+
 {
-	int (*ptrfunc)(va_list, flag_t *);
-	const char *p;
-	va_list args;
-	flag_t flags = {0, 0, 0};
 
-	register int count = 0;
+	int i = 0;
+
+	print_t p[] = {
+
+		{"c", print_c},
+
+		{"s", print_s},
+
+		{NULL, NULL}
+
+	};
 
 
-	va_start(args, format);
 
-	if(!format || (format[0] == '%' && !format[1]))
+	for (; p[i].t != NULL; i++)
+
 	{
-		return (-1);
+
+		if (*(p[i].t) == *format)
+
+			break;
+
 	}
-	if(format[0] == '%' && format[1] == ' ' && !format[2])
-	{
+
+	return (p[i].f);
+
+}
+
+
+
+/**
+
+ * _printf - function to print format
+
+ * @format: list of arguments to printing
+
+ * Return: Number of characters to printing
+
+ */
+
+
+
+int _printf(const char *format, ...)
+
+{
+
+	unsigned int i = 0, len = 0, ibuf = 0;
+
+	va_list arguments;
+
+	int (*function)(va_list, char *, unsigned int);
+
+	char *buffer;
+
+	va_list ap;
+
+	int (*f)(va_list);
+
+	unsigned int i = 0, counter = 0;
+
+
+
+	va_start(arguments, format), buffer = malloc(sizeof(char) * 1024);
+
+	if (!format || !buffer || (format[i] == '%' && !format[i + 1]))
+
+	if (format == NULL)
+
 		return (-1);
-	}
-	for (p = format; *p; p++)
+
+	if (!format[i])
+
+		return (0);
+
+	for (i = 0; format && format[i]; i++)
+
+
+
+	va_start(ap, format);
+
+	while (format && format[i])
+
 	{
-		if (*p == '%')
+
+		if (format[i] == '%')
+
+		if (format[i] != '%')
+
 		{
-			p++;
-			if (*p == '%')
-			{
-				count +=_putchar('%');
-				continue;
-			}
-			/*ptrfunc = get_print(*p);*/
-			count += (ptrfunc)
-				? ptrfunc(args, &flags)
-				: _printf("%%%c", *p);
+
+			if (format[i + 1] == '\0')
+
+			_putchar(format[i]);
+
+			counter++;
+
+			i++;
+
+			continue;
+
 		}
+
 		else
+
 		{
-			count += _putchar(*p);
+
+			if (format[i + 1] == '%')
+
+			{
+
+				print_buf(buffer, ibuf), free(buffer), va_end(arguments);
+
+				return (-1);
+
+				_putchar('%');
+
+				counter++;
+
+				i += 2;
+
+				continue;
+
+			}
+
+			else
+
+			{
+
+				function = get_print_func(format, i + 1);
+
+				if (function == NULL)
+
+				{
+
+					if (format[i + 1] == ' ' && !format[i + 2])
+
+						return (-1);
+
+					handl_buf(buffer, format[i], ibuf), len++, i--;
+
+				}
+
+				else
+
+				{
+
+					len += function(arguments, buffer, ibuf);
+
+					i += ev_print_func(format, i + 1);
+
+				}
+
+			} i++;
+
+				f = check_format(&format[i + 1]);
+
+				if (f == NULL)
+
+					return (-1);
+
+				i += 2;
+
+				counter += f(ap);
+
+				continue;
+
+			}
+
 		}
+
+		else
+
+			handl_buf(buffer, format[i], ibuf), len++;
+
+		for (ibuf = len; ibuf > 1024; ibuf -= 1024)
+
+			;
+
+		i++;
+
 	}
-	_putchar(-1);
-	va_end(args);
-	return (count);
+
+	print_buf(buffer, ibuf), free(buffer), va_end(arguments);
+
+	return (len);
+
+}			
+
+	va_end(ap);
+
+	return (counter);
+
 }
