c7a769ae3101a299fac04d2d621b3cd1aaee2b2f
teddykarisa <teddykarisachaitkc@gmail.com>
2022-10-23 08:16:48 -0700

    the task coreected

---
+++ print_c.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+
+
+/**
+
+ * print_c - prints a char
+
+ * @c: character to print
+
+ * Return: Always 1 (success)
+
+ */
+
+
+
+int print_c(va_list c)
+
+{
+
+	char character = (char)va_arg(c, int);
+
+
+
+	_putchar(character);
+
+	return (1);
+
+}
