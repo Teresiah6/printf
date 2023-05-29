83e995392bfad6fa8f1ae19b4bbca92429eb78f8
Teresiah6 <wangariteresiah6@gmail.com>
2023-05-29 21:29:24 +0300

    task 0

--- printf.c
+++ printf.c
@@ -7,45 +7,51 @@
  */
 int _printf(const char *format, ...)
 {
-	int i = 0, j = 0, buff_count = 0, prev_buff_count = 0;
+	 va_list args;
+    va_start(args, format);
 
-	char buffer[1024];
+    int printed_chars = 0;
+    char ch;
+    const char *str;
 
-	va_list vars;
+    while (*format != '\0')
+    {
+        if (*format == '%')
+        {
+            format++;
 
-	formspec fspec[] = {{'s', prString}, {'c', prChar}, {'X', prX},
-		{'u', prUint}, {'o', prOct}, {'x', prHex}, {'b', prBinary},
-		{'%', prPercent}, {'d', prInt}, {'i', prInt}, {'S', prS},
-		{'r', prReverse}, {'R', prRot}, {'p', prP}, {'\0', NULL}
-	};
-	va_start(vars, format);
-	if (!format)
-		return (-1);
+            if (*format == '%')
+            {
+                putchar('%');
+                printed_chars++;
+            }
+            else if (*format == 'c')
+            {
+                ch = (char)va_arg(args, int);
+                putchar(ch);
+                printed_chars++;
+            }
+            else if (*format == 's')
+            {
+                str = va_arg(args, const char *);
+                while (*str != '\0')
+                {
+                    putchar(*str);
+                    str++;
+                    printed_chars++;
+                }
+            }
+        }
+        else
+        {
+            putchar(*format);
+            printed_chars++;
+        }
 
-	while (format && format[i] != '\0')
-	{
-		if (format[i] == '%')
-		{
-			i++, prev_buff_count = buff_count;
-			for (j = 0; fspec[j].type != '\0'; j++)
-			{
-				if (fspec[j].type == '\0')
-					break;
-				if (format[i] == fspec[j].type)
-				{
-					buff_count = fspec[j].func(buffer, vars, buff_count);
-					break;
-				}
-			}
-			if (buff_count == prev_buff_count && format[i])
-				i--, buffer[buff_count] = format[i], buff_count++;
-		}
-		else
-			buffer[buff_count] = format[i], buff_count++;
-		i++;
-	}
-	va_end(vars);
-	buffer[buff_count] = '\0';
-	print_buff(buffer, buff_count);
-	return (buff_count);
+        format++;
+    }
+
+    va_end(args);
+
+    return printed_chars;
 }
