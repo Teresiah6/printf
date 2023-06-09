f82f7c89e508cd1f62de6a1a83e11ce8b29487b5
Teresiah6 <wangariteresiah6@gmail.com>
2023-06-09 14:44:53 +0300

    addressing some changes with betty

--- _printf.c
+++ _printf.c
@@ -10,55 +10,52 @@
 
 int _printf(const char *format, ...)
 {
-    va_list args;
+	va_list args;
 
-    int count = 0;
+	int count = 0;
 
-    va_start(args, format);
+	va_start(args, format);
 
-    while (*format != '\0')
-    {
-        if (*format == '%')
-        {
-            format++;
-
-            switch (*format)
-            {
-                case 'c':
-                {
-                    char c = (char)va_arg(args, int);
-                    putchar(c);
-                    count++;
-                    break;
-                }
-                case 's':
-                {
-                    char *str = va_arg(args, char*);
-                    while (*str != '\0')
-                    {
-                        putchar(*str);
-                        count++;
-                        str++;
-                    }
-                    break;
-                }
-                case '%':
-                {
-                    putchar('%');
-                    count++;
-                    break;
-                }
-            }
-        }
-        else
-        {
-            putchar(*format);
-            count++;
-        }
-
-        format++;
-    }
-
-    va_end(args);
-    return count;
+	while (*format != '\0')
+	{
+		if (*format == '%')
+		{
+			format++;
+			switch (*format)
+			{
+				case 'c':
+					{
+						char c = (char)va_arg(args, int);
+						putchar(c);
+						count++;
+						break;
+					}
+				case 's':
+					{
+						char *str = va_arg(args, char*);
+						while (*str != '\0')
+						{
+							putchar(*str);
+							count++;
+							str++;
+						}
+						break;
+					}
+				case '%':
+					{
+						putchar('%');
+						count++;
+						break;
+					}
+			}
+		}
+		else
+		{
+			putchar(*format);
+			count++;
+		}
+		format++;
+	}
+	va_end(args);
+	return (count);
 }
